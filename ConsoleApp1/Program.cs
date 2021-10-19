7343d7c43cf015d26025b4d4e031290e7e8590ea
hasanhuseyinyay <hasanhuseyinyay@gmail.com>
2021-10-19 23:49:52 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayı1=1;
+            while (sayı1<=20)
+            {
+               if (sayı1 %2==0)
+                {
+                Console.WriteLine(sayı1);
+
+                } 
+                sayı1++;
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
